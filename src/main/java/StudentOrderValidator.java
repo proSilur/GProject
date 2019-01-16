a20a48837d9b4820683ef5254961ab824c79b8f8
proSilur <prosilur@gmail.com>
2019-01-16 13:59:44 +0200

    Initial commit

---
+++ src/main/java/StudentOrderValidator.java
@@ -0,0 +1,29 @@
+public class StudentOrderValidator {
+    public static void main(String[] args) {
+        checkAll();
+    }
+
+    static void checkCityRegister() {
+        System.out.println("checkCityRegister is running");
+    }
+
+    static void checkWedding() {
+        System.out.println("Wedding is running");
+
+    }
+
+    static void checkChildren() {
+        System.out.println("Children is running");
+    }
+
+    static void checkStudent() {
+        System.out.println("Студенты проверяются");
+    }
+
+    static void checkAll() {
+        checkCityRegister();
+        checkWedding();
+        checkChildren();
+        checkStudent();
+    }
+}
