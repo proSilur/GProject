451859a5b73e64d65bfab488e0d23a8474aa6c40
proSilur <prosilur@gmail.com>
2019-01-17 14:23:38 +0200

    1

--- src/main/java/StudentOrderValidator.java
+++ src/main/java/StudentOrderValidator.java
@@ -3,27 +3,45 @@ public class StudentOrderValidator {
         checkAll();
     }
 
-    static void checkCityRegister() {
-        System.out.println("checkCityRegister is running");
-    }
-
-    static void checkWedding() {
-        System.out.println("Wedding is running");
-
-    }
-
-    static void checkChildren() {
-        System.out.println("Children is running");
-    }
-
-    static void checkStudent() {
-        System.out.println("Студенты is running");
-    }
-
     static void checkAll() {
-        checkCityRegister();
-        checkWedding();
-        checkChildren();
-        checkStudent();
+        StudentOrder so = readStudentOrder();
+
+        AnswerCityRegister cityAnswer = checkCityRegister(so);
+        AnswerWedding answerWedding = checkWedding(so);
+        AnswerChildren answerChildren = checkChildren(so);
+        AnswerStudent answerStudent = checkStudent(so);
+
+        sendMail(so);
     }
+
+    static void sendMail(StudentOrder studentOrder) {
+
+    }
+
+    static StudentOrder readStudentOrder() {
+        return new StudentOrder();
+    }
+
+    static AnswerCityRegister checkCityRegister(StudentOrder studentOrder) {
+        System.out.println("checkCityRegister is running");
+        AnswerCityRegister ans = new AnswerCityRegister();
+        return ans;
+    }
+
+    static AnswerWedding checkWedding(StudentOrder studentOrder) {
+        System.out.println("checkWedding is running");
+        return new AnswerWedding();
+    }
+
+    static AnswerChildren checkChildren(StudentOrder studentOrder) {
+        System.out.println("checkChildren is running");
+        return new AnswerChildren();
+    }
+
+    static AnswerStudent checkStudent(StudentOrder studentOrder) {
+        System.out.println("checkStudent is running");
+        return new AnswerStudent();
+    }
+
+
 }
